4179c9f6b0d343f1ebdb65dd874ec2eff9523893
Chris Davies <chris.davies@theironyard.com>
2015-02-21 15:51:20 -0500

    Removing the focuson directive

--- src/components/directives/focus-on-directive.js
+++
@@ -1,9 +0,0 @@
-// A directive to set focus on an element
-// when some condition is true...
-app.directive('focusOn', function() {
-  return function($scope, elem, attr) {
-    $scope.$on(attr.focusOn, function(e) {
-      elem[0].focus();
-    });
-  };
-});
